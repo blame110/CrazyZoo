1462e759b50d135a4252c0032150fe3cfc11709b
blame110 <46011863+blame110@users.noreply.github.com>
2025-04-28 10:15:19 +0200

    model basico

---
+++ src/main/java/FxApp/model/AnimalDAO.java
@@ -0,0 +1,144 @@
+package FxApp.model;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AnimalDAO {
+
+	/**
+	 * La función devuelve todos los animales de la bd
+	 * @param con Conexion activa a la bd
+	 * @return un ResultSet cargado con los animales
+	 */
+	public static ResultSet getAnimales(Connection con) {
+
+		try {
+			//Con la conexion ya activa (con) crea una sentencia para poder ejecutar
+			//sentencias sql
+			Statement stmt = con.createStatement();
+
+			//Directamente ejecutamos la sentencia
+			//Si es una sentenci tipo select se hace con executeQuery
+			//Esta nos devuelve un objeto de tipo ResultSet con los datos de la query
+			ResultSet rs = stmt.executeQuery("select * from animal");
+
+			return rs;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * La función devuelve el animal asociado a la id que introducimos
+	 * @param con Conexion activa a la bd
+	 * @return un ResultSet cargado con el animal
+	 */
+	public static ResultSet getAnimal(Connection con, int id) {
+
+		try {
+
+			//Para evitar la inyeccion sql los parametros de la query los metemos con ?
+			//Y se los añadiremos despues de forma controlada utilizando preparedStatement
+			String query = "select * from animal where idanimal = ?";
+
+			PreparedStatement pstmt = con.prepareStatement(query);
+
+			//Asignamos el parametro id en la primera interrogacion ( y la unica)
+			pstmt.setInt(1, id);
+
+			//Una vez asignados los valores ejecutamos la query
+			ResultSet rs = pstmt.executeQuery();
+
+			return rs;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * La función elimina el animal asociado a la id que introducimos
+	 * @param con Conexion activa a la bd
+	 * @return 1 si ha podido borrar al animal
+	 */
+	public static int deleteAnimal(Connection con, int id) {
+
+		try {
+
+			//Para evitar la inyeccion sql los parametros de la query los metemos con ?
+			//Y se los añadiremos despues de forma controlada utilizando preparedStatement
+			String query = "delete from animal where idanimal = ?";
+
+			PreparedStatement pstmt = con.prepareStatement(query);
+
+			//Asignamos el parametro id en la primera interrogacion ( y la unica)
+			pstmt.setInt(1, id);
+
+			//Una vez asignados los valores ejecutamos la query
+			//update me devuelve la cantidad de registros afectados por la query
+			int resultado = pstmt.executeUpdate();
+
+			return resultado;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Funcion que recibe una conexion y los datos del animal con un animalDO
+	 * Y inserta en BD el registro
+	 * @param con Conexión activa a la BD
+	 * @param animal objeto de la clase AnimalDO con los datos a insertar
+	 * @return -1 si falla o no hay datos y 1 si ha podido insertar el registro en BD
+	 */
+	public static int insertAnimal(Connection con, AnimalDO animal) {
+		//Si el animal esta vacio devolvemos error con un -1
+		if (animal == null)
+			return -1;
+
+		try {
+
+			//Para evitar la inyeccion sql los parametros de la query los metemos con ?
+			//Y se los añadiremos despues de forma controlada utilizando preparedStatement
+			String query = "INSERT INTO `CrazyZoo`.`animal` "
+					+ "(`nombre`, `especie`, `fecha_nac`, `sexo`, `Jaula_idJaula`) " + "VALUES (?,?,?,?,?);";
+
+			PreparedStatement pstmt = con.prepareStatement(query);
+
+			//Asignamos el parametro id en la primera interrogacion ( y la unica)
+			pstmt.setString(1, animal.getNombre());
+			pstmt.setString(2, animal.getEspecie());
+			pstmt.setDate(3, (Date) animal.getFecha_nac());
+			pstmt.setString(4, Character.toString(animal.getSexo()));
+			pstmt.setInt(5, animal.getJaula_idJaula());
+
+			//Una vez asignados los valores ejecutamos la query
+			//update me devuelve la cantidad de registros afectados por la query
+			int resultado = pstmt.executeUpdate();
+
+			return resultado;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+
+		}
+
+	}
+
+}
